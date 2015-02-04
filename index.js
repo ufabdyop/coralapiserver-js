9e0eee26381dc4ba40cdf67271c67c3c55991fbb
Ryan Taylor <ryant@nanofab.utah.edu>
2015-02-04 14:06:28 -0700

    update

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 "use strict";
-document.write('<p>Hello World</p>');
+// Multiline Function String - Nate Ferrero - Public Domain
+function heredoc (f) {
+    return f.toString().match(/\/\*\s*([\s\S]*?)\s*\*\//m)[1];
+};
+
+var txt = heredoc(function()
+{/*
+A test of horrible
+Multi-line strings!
+<script src="https://code.jquery.com/jquery-1.11.2.js"></script>
+*/});
+
+document.write(txt);
