3a874e0161599da5597a662fd0a9d56fe1b63c60
Ryan Taylor <ryant@nanofab.utah.edu>
2015-02-04 14:25:55 -0700

    incremental

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ var txt = heredoc(function()
 {/*
 <script src="https://code.jquery.com/jquery-1.11.2.js"></script>
 <h3>Version</h3>
-<p>The simplest API call is to ask the API what version it is running.</p>
+<p>The simplest API call is to ask the API what version it is running.  With curl, this is done as:
+<br/>
+curl http://SERVERNAMEANDPORT/version
+</p>
 
 <h3>Authentication</h3>
 <p>There are a few ways to authenticate to coral api server.  The simplest is a basic http auth with 
